cefc15fc1c80a1a3a51f9ab02110351e76db3cbf
IlyasDiker <ilyasbenhssine@gmail.com>
2021-10-26 00:43:02 +0100

    add some stuff

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <div>
-    <h1>tedsasd</h1>
-  </div>
+	<Navbar></Navbar>
+	<div class="container">
+		<div class="content">
+			<h1>tedsasd</h1>
+		</div>
+	</div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    //HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Navbar
+
+	},
+};
 </script>
 
-<style>
+<style lang="less">
+// I Always reset everything so i have controle over everything 😈
+*{
+	margin: 0px;
+	padding: 0px;
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
+}
 
+.container{
+	height: 100vh;
+	width: 100vw;
+	overflow-y: auto;
+	overflow-x: hidden;
+	background: black;
+	color: white;
+}
+
+.content{
+	margin-top:80px;
+	color: white;
+}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar">
+        <span class="navbar-logo">Gemophraphy-frontend-coding-challenge</span>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    
+}
+</script>
+
+<style lang="less">
+    .navbar{
+        position: fixed;
+        top: 0;
+        right: 0;
+        left: 0;
+        padding: 20px;
+        color: white;
+        .navbar-logo{
+            font-size: 2rem;
+        }
+        
+    }
+</style>
